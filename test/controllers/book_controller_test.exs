2242e41a5368bedeb5d60cbaa0de4aa17368fbbf
Pawel Dawczak <pawel.dawczak@gmail.com>
2016-05-20 07:09:30 +0100

    Update tests to use factories

--- test/controllers/book_controller_test.exs
+++ test/controllers/book_controller_test.exs
@@ -1,9 +1,8 @@
 defmodule SL.BookControllerTest do
   use SL.ConnCase
+  import SL.Factory
 
   alias SL.Book
-  @valid_attrs %{description: "some content", image: "some content", isbn: "some content", title: "some content"}
-  @invalid_attrs %{}
 
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, book_path(conn, :index)
@@ -16,20 +15,20 @@ defmodule SL.BookControllerTest do
   end
 
   test "creates resource and redirects when data is valid", %{conn: conn} do
-    conn = post conn, book_path(conn, :create), book: @valid_attrs
+    conn = post conn, book_path(conn, :create), book: fields_for(:book, title: "The Book")
     assert redirected_to(conn) == book_path(conn, :index)
-    assert Repo.get_by(Book, @valid_attrs)
+    assert Repo.get_by(Book, title: "The Book")
   end
 
   test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, book_path(conn, :create), book: @invalid_attrs
+    conn = post conn, book_path(conn, :create), book: fields_for(:book, title: nil)
     assert html_response(conn, 200) =~ "New book"
   end
 
   test "shows chosen resource", %{conn: conn} do
-    book = Repo.insert! %Book{}
+    book = create(:book, title: "Sample book")
     conn = get conn, book_path(conn, :show, book)
-    assert html_response(conn, 200) =~ "Show book"
+    assert html_response(conn, 200) =~ "Sample book"
   end
 
   test "renders page not found when id is nonexistent", %{conn: conn} do
@@ -39,26 +38,26 @@ defmodule SL.BookControllerTest do
   end
 
   test "renders form for editing chosen resource", %{conn: conn} do
-    book = Repo.insert! %Book{}
+    book = create(:book)
     conn = get conn, book_path(conn, :edit, book)
     assert html_response(conn, 200) =~ "Edit book"
   end
 
   test "updates chosen resource and redirects when data is valid", %{conn: conn} do
-    book = Repo.insert! %Book{}
-    conn = put conn, book_path(conn, :update, book), book: @valid_attrs
+    book = create(:book, title: "Sample Book")
+    conn = put conn, book_path(conn, :update, book), book: %{title: "The Book"}
     assert redirected_to(conn) == book_path(conn, :show, book)
-    assert Repo.get_by(Book, @valid_attrs)
+    assert Repo.get_by(Book, title: "The Book")
   end
 
   test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    book = Repo.insert! %Book{}
-    conn = put conn, book_path(conn, :update, book), book: @invalid_attrs
+    book = create(:book)
+    conn = put conn, book_path(conn, :update, book), book: fields_for(:book, title: nil)
     assert html_response(conn, 200) =~ "Edit book"
   end
 
   test "deletes chosen resource", %{conn: conn} do
-    book = Repo.insert! %Book{}
+    book = create(:book)
     conn = delete conn, book_path(conn, :delete, book)
     assert redirected_to(conn) == book_path(conn, :index)
     refute Repo.get(Book, book.id)
--- test/models/book_test.exs
+++ test/models/book_test.exs
@@ -1,18 +1,16 @@
 defmodule SL.BookTest do
   use SL.ModelCase
+  import SL.Factory
 
   alias SL.Book
 
-  @valid_attrs %{description: "some content", image: "some content", isbn: "some content", title: "some content"}
-  @invalid_attrs %{}
-
   test "changeset with valid attributes" do
-    changeset = Book.changeset(%Book{}, @valid_attrs)
+    changeset = Book.changeset(%Book{}, fields_for(:book))
     assert changeset.valid?
   end
 
   test "changeset with invalid attributes" do
-    changeset = Book.changeset(%Book{}, @invalid_attrs)
+    changeset = Book.changeset(%Book{}, fields_for(:book, title: nil))
     refute changeset.valid?
   end
 end
