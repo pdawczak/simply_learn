ccd57c856b6a411524a6571580e5df25c0d0b84a
Pawel Dawczak <pawel.dawczak@gmail.com>
2016-05-23 15:24:44 +0100

    Update index page for Books

--- web/views/book_view.ex
+++ web/views/book_view.ex
@@ -1,3 +1,15 @@
 defmodule SL.BookView do
   use SL.Web, :view
+
+  alias SL.Book
+
+  def shorter_description(%Book{description: nil}),
+  do: ""
+
+  @length 250
+  def shorter_description(%Book{description: description}) when byte_size(description) > @length,
+  do: String.slice(description, 0, @length) <> "..."
+
+  def shorter_description(%Book{description: description}),
+  do: description
 end
