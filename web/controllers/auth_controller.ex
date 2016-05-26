65791c4023a5c9f6d8f9cf4ea0e71274dd1d7e66
Pawel Dawczak <pawel.dawczak@gmail.com>
2016-05-26 21:05:12 +0100

    Extrac feed for join

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -30,26 +30,12 @@ defmodule SL.AuthController do
   defp handle_oauth_success(conn, %User{id: nil} = non_persisted_user) do
     user = Repo.insert!(non_persisted_user)
 
+    SL.Feed.Broadcast.new_join(user)
+
     conn
     |> put_session(:user_id, user.id)
     |> put_flash(:info, "Hi #{user.first_name}, nice to have you on board!")
-    |> broadcast_new_join(user)
     |> redirect(to: feed_path(conn, :index))
-  end
-
-  defp broadcast_new_join(conn, %{first_name: first_name, last_name: last_name}) do
-    {:ok, feed} =
-      %SL.Feed{
-        title: "New join",
-        content: "*#{first_name} #{last_name}* has just joined!"
-      }
-      |> Repo.insert
-
-    feed_json = Phoenix.View.render(SL.FeedView, "feed.json", %{feed: feed})
-
-    SL.Endpoint.broadcast("feed:lobby", "new_feed", %{feed: feed_json})
-
-    conn
   end
 
   defp handle_oauth_success(conn, user) do
---
+++ web/feed/broadcast.ex
@@ -0,0 +1,18 @@
+defmodule SL.Feed.Broadcast do
+  alias SL.{Endpoint, Feed, FeedView, Repo}
+  alias Phoenix.View
+
+  def new_join(%SL.User{first_name: first_name, last_name: last_name}) do
+    %Feed{title: "New join",
+          content: "*#{first_name} #{last_name}* has just joined!"}
+    |> handle
+  end
+
+  defp handle(feed) do
+    feed_json = View.render(FeedView,
+                            "feed.json",
+                            %{feed: Repo.insert!(feed)})
+
+    Endpoint.broadcast("feed:lobby", "new_feed", %{feed: feed_json})
+  end
+end
