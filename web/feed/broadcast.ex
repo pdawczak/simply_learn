8554c985dd893ae76c31bf872c027df499f0dd87
Pawel Dawczak <pawel.dawczak@gmail.com>
2016-05-27 08:18:45 +0100

    Associate user before broadcasting the feed

--- web/feed/broadcast.ex
+++ web/feed/broadcast.ex
@@ -2,47 +2,52 @@ defmodule SL.Feed.Broadcast do
   alias SL.{Endpoint, Feed, FeedView, Repo}
   alias Phoenix.View
 
-  def new_join(%SL.User{id: user_id, first_name: first_name, last_name: last_name}) do
+  def new_join(user = %SL.User{id: user_id, first_name: first_name, last_name: last_name}) do
     %Feed{
       title: "New join",
       content: "*#{first_name} #{last_name}* has just joined!",
       user_id: user_id
     }
-    |> handle
+    |> handle(user)
   end
 
-  def new_book(%SL.Book{title: title}, link, %SL.User{id: user_id}) do
+  def new_book(%SL.Book{title: title}, link, user = %SL.User{id: user_id}) do
     %Feed{
       title: "New book",
       content: "*#{title}* has been added.",
       link: link,
       user_id: user_id
     }
-    |> handle
+    |> handle(user)
   end
 
-  def book_borrowed(%SL.Book{title: title}, %SL.User{id: user_id, first_name: first_name, last_name: last_name}) do
+  def book_borrowed(%SL.Book{title: title}, user = %SL.User{id: user_id, first_name: first_name, last_name: last_name}) do
     %Feed{
       title: "Book borrowed",
       content: "*#{first_name} #{last_name}* has just borrowed copy of *#{title}*",
       user_id: user_id
     }
-    |> handle
+    |> handle(user)
   end
 
-  def book_returned(%SL.Book{title: title}, %SL.User{id: user_id, first_name: first_name, last_name: last_name}) do
+  def book_returned(%SL.Book{title: title}, user = %SL.User{id: user_id, first_name: first_name, last_name: last_name}) do
     %Feed{
       title: "Book returned",
       content: "*#{first_name} #{last_name}* has just returned copy of *#{title}*",
       user_id: user_id
     }
-    |> handle
+    |> handle(user)
   end
 
-  defp handle(feed) do
+  defp handle(feed, user) do
+    feed =
+      feed
+      |> Repo.insert!
+      |> Map.put(:user, user)
+
     feed_json = View.render(FeedView,
                             "feed.json",
-                            %{feed: Repo.insert!(feed)})
+                            %{feed: feed})
 
     Endpoint.broadcast("feed:lobby", "new_feed", %{feed: feed_json})
   end
